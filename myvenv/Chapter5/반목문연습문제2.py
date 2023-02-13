d26f26836e74581b67cdb0f1484d2a593f7df0e9
ohsangsoo <tkdtn3264@gmail.com>
2023-02-13 19:54:05 +0900

    반복문

--- myvenv/Chapter5/반목문연습문제2.py
+++ myvenv/Chapter5/반목문연습문제2.py
@@ -1,12 +1,14 @@
-
+cash=int(input("현질금액할 금액을 입력해주세요"))
 while True:
     print("[메뉴을 선택해주세요]")
-    game_menu = (int(input("1.게임 시작 2.랭킹보기 3. 게임종료>>>")))
+    game_menu = (int(input("1.게임 시작 2.랭킹보기 3.캐쉬금액 확인하기  4. 게임종료>>>")))
     if game_menu== 1:
         print("->게임을 시작합니다.")
     elif game_menu ==2:
         print("실시간 랭킹")
-    elif game_menu ==3:
+    elif game_menu == 3:
+        print(f"₩{cash}")  
+    elif game_menu ==4:
         break
         print("게임을 종료합니다.")  
     else:
