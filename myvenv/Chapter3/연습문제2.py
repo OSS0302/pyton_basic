d623290a0bc186b71487f59c89cc0a2cd1122a04
ohsangsoo <tkdtn3264@gmail.com>
2023-02-13 17:31:47 +0900

    나이입력하면 생월월일 작업

--- myvenv/Chapter3/연습문제2.py
+++ myvenv/Chapter3/연습문제2.py
@@ -1,3 +1,8 @@
 year = int(input("태어난 연도를 입력하세요>>>"))
 age = 2023 -year
 print(f"나이는",age,"입니다.")
+# 생년월일 ->나이구하기 
+
+age_born = int(input("나이를 입력해주세요"))
+born_year = 2023- age_born
+print(f"생년월일{born_year}년입니다") 
