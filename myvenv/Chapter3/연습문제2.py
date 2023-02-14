6cd86aef4efe1112a0d8b5d239f56508b3afe8b7
ohsangsoo <tkdtn3264@gmail.com>
2023-02-14 16:21:48 +0900

    조건문 연습하기

--- myvenv/Chapter3/연습문제2.py
+++ myvenv/Chapter3/연습문제2.py
@@ -6,3 +6,15 @@ print(f"나이는",age,"입니다.")
 age_born = int(input("나이를 입력해주세요"))
 born_year = 2023- age_born
 print(f"생년월일{born_year}년입니다") 
+
+if age <=26 and age >=20:
+    print("대학생")
+elif age <20 and age>=17:
+    print("고등학생이다.")
+elif age < 17  and age >=14:
+    print("중학생")
+elif age < 14 and age >=8:
+    print("초등학생")
+else: 
+    print("학생이 아닙니다.") 
+    
